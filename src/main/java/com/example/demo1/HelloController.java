a2465c69df193c1a471e9927b1898eaa7a7e1715
Fluffy96 <52479039+Fluffy96@users.noreply.github.com>
2021-10-27 16:51:30 -0400

    Stu Profile

--- src/main/java/com/example/demo1/HelloController.java
+++ src/main/java/com/example/demo1/HelloController.java
@@ -1,14 +1,19 @@
 package com.example.demo1;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 
 public class HelloController {
     @FXML
-    private Label welcomeText;
+    private Button addStudent;
+    @FXML
+    private TextField studentProfileOutput;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    protected void addClicked() {
+        studentProfileOutput.setText("HI");
+
     }
 }
