6fab3dd52f85fc51c82e207cc6f4e049ae9812bd
DivyeshBaskaran <divyesh.baskaran@gmail.com>
2021-10-28 22:01:08 -0400

    10/28 10:00pm

--- src/main/java/com/example/demo1/HelloController.java
+++ src/main/java/com/example/demo1/HelloController.java
@@ -2,6 +2,8 @@ package com.example.demo1;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.*;
+import com.example.demo1.Student.Major;
+import com.example.demo1.Student.Tri;
 
 public class HelloController {
     @FXML
@@ -9,9 +11,20 @@ public class HelloController {
     @FXML
     private Button addStudent;
     @FXML
-    private TextField profileOutput, inputName;
+    private TextField inputName, creditHours;
     @FXML
-    private ToggleGroup Majors,rVSnr;
+    private TextArea profileOutput;
+    @FXML
+    private ToggleGroup Majors, Students, State;
+    @FXML
+    private RadioButton isRes, isNonRes, isTristate, isNY, isCT, isInternational;
+    @FXML
+    private CheckBox isAbroad;
+
+    private static final double INVISIBLE = 0.60;
+    private static final double VISIBLE = 1.00;
+
+    private Roster roster = new Roster();
 
     @FXML
     protected void onHelloButtonClick() {
@@ -20,19 +33,35 @@ public class HelloController {
     @FXML
     protected void onAddClick() {
         String name = inputName.getText();
+        profileOutput.setText("");
         if(!name.equals("")) {
             String maj = returnMajor();
             if (maj != null) {
-                String resType = returnRes();
-                if(resType!= null){
-                    if(resType.equals("Resident")){
+                Major major = toMajor(maj);
+                String studentType = returnStudent();
+                if(studentType!= null){
+                    int credits = returnCredit();
+                    if(credits != -1 && creditValidator(credits)){
+                        if(studentType.equals("Resident")){
+                            roster.add(new Resident(name,major,credits));
+                        }else if(studentType.equals("Non-Resident")){
+                            roster.add(new NonResident(name,major,credits));
+                        }else if(studentType.equals("Tristate")){
+                            if(returnState()!=null){
+                                roster.add(new TriState(name,major,credits,returnState()));
+                            }else{
+                                profileOutput.setText("Have not selected State");
+                            }
+                        }else if(studentType.equals("International")){
 
-                    }else{
-                        //check for international or tristate followed by check for credits
+                        }
+                    }
+                    else{
+                        profileOutput.setText("Invalid Credit Hours");
                     }
 
                 }else {
-                    profileOutput.setText("Have not clicked Non Resident or Resident");
+                    profileOutput.setText("Have not clicked Student Type");
                 }
             } else {
                 profileOutput.setText("Have not clicked a Major");
@@ -41,6 +70,93 @@ public class HelloController {
             profileOutput.setText("No name provided");
         }
     }
+    @FXML
+    protected void onResidentClick(){
+        isNY.setOpacity(INVISIBLE);
+        isCT.setOpacity(INVISIBLE);
+        isAbroad.setOpacity(INVISIBLE);
+        isNY.setDisable(true);
+        isCT.setDisable(true);
+        isAbroad.setDisable(true);
+        isTristate.setSelected(false);
+        isNY.setSelected(false);
+        isCT.setSelected(false);
+        isInternational.setSelected(false);
+        isAbroad.setSelected(false);
+    }
+
+    @FXML
+    protected void onNonResidentClick(){
+        isTristate.setOpacity(VISIBLE);
+        isNY.setOpacity(INVISIBLE);
+        isCT.setOpacity(INVISIBLE);
+        isInternational.setOpacity(VISIBLE);
+        isAbroad.setOpacity(INVISIBLE);
+        isTristate.setDisable(false);
+        isInternational.setDisable(false);
+        isNY.setDisable(true);
+        isCT.setDisable(true);
+        isAbroad.setDisable(true);
+        isInternational.setSelected(false);
+        isNY.setSelected(false);
+        isCT.setSelected(false);
+        isAbroad.setSelected(false);
+        isTristate.setSelected(false);
+    }
+
+    @FXML
+    protected void onTriStateClick(){
+        isTristate.setOpacity(VISIBLE);
+        isNY.setOpacity(VISIBLE);
+        isCT.setOpacity(VISIBLE);
+        isInternational.setOpacity(VISIBLE);
+        isAbroad.setOpacity(INVISIBLE);
+        isTristate.setDisable(false);
+        isInternational.setDisable(false);
+        isNY.setDisable(false);
+        isCT.setDisable(false);
+        isAbroad.setDisable(true);
+        isNonRes.setSelected(false);
+        isNY.setSelected(false);
+        isCT.setSelected(false);
+        isAbroad.setSelected(false);
+    }
+
+    @FXML
+    protected void onInternationalClick(){
+        isTristate.setOpacity(VISIBLE);
+        isNY.setOpacity(INVISIBLE);
+        isCT.setOpacity(INVISIBLE);
+        isInternational.setOpacity(VISIBLE);
+        isAbroad.setOpacity(VISIBLE);
+        isTristate.setDisable(false);
+        isInternational.setDisable(false);
+        isNY.setDisable(true);
+        isCT.setDisable(true);
+        isAbroad.setDisable(false);
+        isNonRes.setSelected(false);
+        isNY.setSelected(false);
+        isCT.setSelected(false);
+        isAbroad.setSelected(false);
+    }
+
+    private Tri returnState(){
+        try {
+            RadioButton selectedRadioButton = (RadioButton) State.getSelectedToggle();
+            return(toTriState(selectedRadioButton.getText()));
+        }catch(NullPointerException e){
+            return(null);
+        }
+    }
+
+    private int returnCredit(){
+        try{
+            return Integer.parseInt(creditHours.getText());
+        }catch (NumberFormatException e){
+            return -1;
+        }
+    }
+
     private String returnMajor(){
         try {
             RadioButton selectedRadioButton = (RadioButton) Majors.getSelectedToggle();
@@ -49,9 +165,9 @@ public class HelloController {
             return(null);
         }
     }
-    private String returnRes(){
+    private String returnStudent(){
         try {
-            RadioButton selectedRadioButton = (RadioButton) rVSnr.getSelectedToggle();
+            RadioButton selectedRadioButton = (RadioButton) Students.getSelectedToggle();
             return(selectedRadioButton.getText());
         }catch(NullPointerException e){
             return(null);
@@ -61,4 +177,100 @@ public class HelloController {
     protected void onRemoveClick() {
         profileOutput.setText("HI");
     }
+    /**
+     * Method returns tristate enum based on string input
+     * @param triState
+     * @returns Tri
+     */
+    private Student.Tri toTriState (String triState){
+        switch (triState.toUpperCase()){
+            case "CT":
+                return Student.Tri.CT;
+            case "NY":
+                return Student.Tri.NY;
+            default:
+                System.out.println("Not part of the tri-state area.");
+                return null;
+        }
+    }
+
+    /**
+     * Method returns a major based on input
+     * @param input
+     * @returns major
+     */
+    private Student.Major toMajor (String input){
+        switch (input.toUpperCase()){
+            case "EE":
+                return Student.Major.EE;
+            case "CS":
+                return Student.Major.CS;
+            case "BA":
+                return Student.Major.BA;
+            case "IT":
+                return Student.Major.IT;
+            case "ME":
+                return Student.Major.ME;
+            default:
+                System.out.println(input+" is not a valid major.");
+                return null;
+        }
+    }
+
+    /**
+     * Method returns boolean based on if credits input is valid
+     * @param credits
+     * @returns boolean
+     */
+    private boolean creditValidator (int credits){
+        if (credits < 0){
+            System.out.println("Credit hours cannot be negative.");
+            return false;
+        }
+        else if (credits < Student.MIN_CREDITS){
+            System.out.println("Minimum credit hours is 3.");
+            return false;
+        }
+        else if (credits > Student.MAX_CREDITS){
+            System.out.println("Credit hours exceed the maximum 24.");
+            return false;
+        }
+        else{
+            return true;
+        }
+    }
+    /**
+     * Method returns boolean based on if credits input is valid for international students
+     * @param credits
+     * @returns boolean
+     */
+    private boolean creditValidator (int credits, boolean isStudyAbroad){
+        if (credits < 0){
+            System.out.println("Credit hours cannot be negative.");
+            return false;
+        }
+        else if (credits < Student.MIN_CREDITS){
+            System.out.println("Minimum credit hours is 3.");
+            return false;
+        }
+        else if(!isStudyAbroad) {
+            if (credits < Student.MAX_PARTTIME_CREDITS) {
+                System.out.println("International students must enroll at least 12 credits.");
+                return false;
+            } else if (credits > Student.MAX_CREDITS) {
+                System.out.println("Credit hours exceed the maximum 24.");
+                return false;
+            } else {
+                return true;
+            }
+        }
+        else if(credits > Student.MAX_PARTTIME_CREDITS){
+            System.out.println("Credit hours exceed the maximum 12.");
+            return false;
+        }
+        else{
+            return true;
+        }
+    }
+
 }
